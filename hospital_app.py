2d0a58bb4d7745a9fb013ae6d9c36434b8c2f73c
idkwhatsmyn4me <hellyncallystahuang@gmail.com>
2026-06-27 10:06:29 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-st.set_page_config(page_title="Smart Hospital Ptiemnt Navigator", page_icon"🏥")
+st.set_page_config(page_title="Smart Hospital Ptiemnt Navigator", page_icon="🏥")
